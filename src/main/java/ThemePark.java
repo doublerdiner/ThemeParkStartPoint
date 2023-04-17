9daab5cbf0f7afb933db1393ed1d1124605d21cf
Chris Barclay <chris.barclay1@gmail.com>
2023-04-17 22:29:54 +0100

    Advanced Extensions Completed

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,20 +1,15 @@
 import attractions.*;
 import behaviours.IReviewed;
+import behaviours.ISecurity;
 import people.Visitor;
 import stalls.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ThemePark {
     private ArrayList<Attraction> attractions;
     private ArrayList<Stall> stalls;
-//    Dodgems dodgems = new Dodgems("Dodgems", 8);
-//    Park park = new Park("Peaks Park", 6);
-//    Playground playground = new Playground("Pleasant Playground", 5);
-//    RollerCoaster rollerCoaster = new RollerCoaster("Runaway Train", 9);
-//    CandyflossStall candyflossStall = new CandyflossStall("Sugar Hit", "Barry", 7, ParkingSpot.A1);
-//    IceCreamStall iceCreamStall = new IceCreamStall("Deep Freeze", "Barry", 10, ParkingSpot.A2);
-//    TobaccoStall tobaccoStall = new TobaccoStall("Smokey Joe's", "Barry", 1, ParkingSpot.A3);
     public ThemePark(){
         this.attractions = new ArrayList<>();
         this.stalls = new ArrayList<>();
@@ -42,10 +37,54 @@ public class ThemePark {
         return response;
     }
     public void visitAttraction(Visitor visitor, Attraction attraction){
-        this.attractions.add(attraction);
-        int index = this.attractions.indexOf(attraction);
-        int currentVisits = this.attractions.get(index).getVisitCount();
-        this.attractions.get(index).setVisitCount(currentVisits + 1);
-        visitor.visitAttraction(attraction);
+        String attractionName = attraction.getName();
+        for(Attraction ride : this.attractions) {
+            if (ride.getName() == attractionName) {
+                int index = this.attractions.indexOf(ride);
+                int currentVisits = this.attractions.get(index).getVisitCount();
+                this.attractions.get(index).setVisitCount(currentVisits + 1);
+                visitor.visitAttraction(ride);
+            }
+        }
     }
+    public HashMap<String, Integer> allReviews(){
+        HashMap<String, Integer> result = new HashMap<>();
+        for(Attraction ride : this.attractions){
+            result.put(ride.getName(), ride.getRating());
+        }
+        for(Stall stall : this.stalls){
+            result.put(stall.getName(), stall.getRating());
+        }
+        return result;
+    }
+    public ArrayList<IReviewed> getAllAllowedFor(Visitor visitor) {
+        ArrayList<IReviewed> response = getAllReviewed();
+        ArrayList<IReviewed> finalAnswer = new ArrayList<>();
+        for(IReviewed activity : response){
+            if(activity.getName().contains("Runaway")){
+                if(((RollerCoaster) activity).isAllowedTo(visitor)) {
+                    finalAnswer.add(activity);
+                    continue;
+                }
+                else{continue;}
+            }
+            if(activity.getName().contains("Pleasant")){
+                if(((Playground) activity).isAllowedTo(visitor)) {
+                    finalAnswer.add(activity);
+                    continue;
+                }
+                else{continue;}
+            }
+            if(activity.getName().contains("Smokey")){
+                if(((TobaccoStall)activity).isAllowedTo(visitor)){
+                    finalAnswer.add(activity);
+                    continue;
+                }
+                else{continue;}
+            }
+            finalAnswer.add(activity);
+        }
+        return finalAnswer;
+    }
+
 }
--- src/test/java/ThemeParkTest.java
+++ src/test/java/ThemeParkTest.java
@@ -1,3 +1,4 @@
+import attractions.Attraction;
 import attractions.RollerCoaster;
 import behaviours.IReviewed;
 import org.junit.Before;
@@ -28,6 +29,23 @@ public class ThemeParkTest {
         visitor = new Visitor(20, 1.7, 30);
         themePark.visitAttraction(visitor, rollerCoaster);
         assertEquals("Runaway Train", visitor.getVisitedAttractions().get(0).getName());
-        assertEquals(1, rollerCoaster.getVisitCount());
+        for(Attraction ride : themePark.getAttractions()){
+            System.out.println(ride.getName());
+            System.out.println(ride.getVisitCount());
+        }
+    }
+    @Test
+    public void allReviewReport(){
+        System.out.println(themePark.allReviews());
+        assertEquals(7, themePark.allReviews().size());
+    }
+    @Test
+    public void checkAvailableAttractions(){
+        Visitor visitor = new Visitor(11, 1.38, 20);
+        ArrayList<IReviewed> answer = themePark.getAllAllowedFor(visitor);
+        assertEquals(5, answer.size());
+        Visitor visitor1 = new Visitor(18, 2.01, 15);
+        ArrayList<IReviewed> answer2 = themePark.getAllAllowedFor(visitor1);
+        assertEquals(6, answer2.size());
     }
 }
